92882b378939a202e6112c79a5032d919438579a
BuildTools <unconfigured@null.spigotmc.org>
2018-03-30 10:16:21 -0400

    ex1

---
+++ calculette.js
@@ -0,0 +1,16 @@
+var quantite = 10
+var prix = 20
+var total = (quantite * prix)
+console.log(total)
+
+
+var item = "pomme"
+var devise = "dollars"
+var listeprix = (item + ":" + prix)
+console.log(listeprix)
+
+if (prix > 20) {
+    console.log("Merci de financer mon abonnement à Playstation plus!");
+} else {
+    console.log("Merci et bonne journée!");
+}
